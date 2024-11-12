2cfffcce9e2f82ae0c102aed64e950a1ecb173d4
LeoSeiti <Leonardoseiti1@gmail.com>
2024-11-12 09:12:33 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/lume/LumeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.lume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/lume/LumeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.lume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
